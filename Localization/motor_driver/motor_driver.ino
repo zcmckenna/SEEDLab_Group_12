bddb6a6bf1ac99a546fc8cb1ee2fda6b9482786f
bntriton08 <91344902+bntriton08@users.noreply.github.com>
2021-10-07 19:38:31 -0600

    Update motor_driver.ino
    Added comments to main localization code for documentation

--- Localization/motor_driver/motor_driver.ino
+++ Localization/motor_driver/motor_driver.ino
@@ -4,21 +4,21 @@
 
 double desiredAngle =0.0;
 
-int inputCLK = 5;                              // CLK input assigned to pin 4
+int inputCLK = 5;                              // CLK input assigned to pin 5
 int inputDT = 3;                              // DT input assigned to pin 3
-int enablePin = 4;
-int M1DIR = 7;
-int M2DIR = 8;
-int M1PWM = 9;
+int enablePin = 4;                            // Tri-state enablePin set to pin 4
+int M1DIR = 7;                                 //Motor 1 direction set to pin 7                       
+int M2DIR = 8;                             
+int M1PWM = 9;                                //Motor 1 PWM input set to pin 9
 int M2PWM = 10;
-int SFlag = 12;
-int resetBtn = 2;
+int SFlag = 12;                               // Status flag indicator set to pin 12
+int resetBtn = 2;                             
 
 int currentStateCLK = LOW;                // variable for current state of the CLK 
 int prevStateCLK = currentStateCLK;      // variable for the previous state of CLK
 volatile float counter = 0;               // variable that counts the number of rotations. -1 for CCW and +1 for CW
-volatile float oldRad = 0;
-volatile float newRad = 0;
+volatile float oldRad = 0;                //variable stores old angular position 
+volatile float newRad = 0;                //variable for new angular position calculated from encoder interrupt                     
 
 volatile float timeDiff = 0;
 
@@ -30,7 +30,7 @@ int dir;
 
 
 void setup() {
-  Serial.begin (230400);                    
+  Serial.begin (230400);                  //high baud rate used since the encoder has a high resolution                   
 
   pinMode (inputCLK, INPUT_PULLUP);               
   pinMode (inputDT, INPUT_PULLUP);  
@@ -41,18 +41,18 @@ void setup() {
   pinMode (M2PWM, OUTPUT);
   pinMode (SFlag, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(inputDT), ISR_encoder, CHANGE);       //attach the ISR to the DT input to check for changes
-  attachInterrupt(digitalPinToInterrupt(resetBtn), ISR_reset, HIGH);
+  attachInterrupt(digitalPinToInterrupt(resetBtn), ISR_reset, HIGH);          //attach an ISR to the push-button pin that is called when it is HIGH
   
-  digitalWrite(enablePin, HIGH);
-  digitalWrite(SFlag, HIGH);
+  digitalWrite(enablePin, HIGH);                        //set the enable pin HIGH 
+  digitalWrite(SFlag, HIGH);                            //set the status flag HIGH
 
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveQuad);
 }
 
 void loop() {
-      if(desiredAngle == 0.0){
-        if((newRad<(6.28-0.02)) && (newRad>(0.0+0.02))){
+      if(desiredAngle == 0.0){                        //runs section if the desired angle is 0
+        if((newRad<(6.28-0.02)) && (newRad>(0.0+0.02))){   //motor is turned on until the current angular position is within 0.02 radians of the desired angle
         digitalWrite(enablePin, HIGH);
         analogWrite(M1PWM,50);
         if((newRad > 0) && (newRad <= 3.14)){
@@ -63,13 +63,13 @@ void loop() {
         }
       }
       else{
-        analogWrite(M1PWM,0);
+        analogWrite(M1PWM,0);                        //runs section if the motor's position is at the desired angle and turns off the motor
         digitalWrite(M1DIR, LOW);
         digitalWrite(enablePin,LOW);
       }
       }
       else{
-        if(newRad<(desiredAngle-0.02) || newRad>(desiredAngle+0.02)){
+        if(newRad<(desiredAngle-0.02) || newRad>(desiredAngle+0.02)){       //runs section for the other possible angular position set points
         digitalWrite(enablePin, HIGH);
         analogWrite(M1PWM,50);
         if(newRad > desiredAngle){
@@ -85,7 +85,6 @@ void loop() {
         digitalWrite(enablePin,LOW);
       }
       }
-     // PIcontroller();
       
       
 }
@@ -96,34 +95,34 @@ void ISR_encoder() {
           
           if (digitalRead(inputDT) == HIGH) {      //if the current state of DT is high, then the encoder must have been turned CW
             counter += 1.0;
-            if(counter > 800.0){
+            if(counter > 800.0){               //resets counter if the number of counts exceeds 1 revolution 
               counter = 0.0;
             }
-            newRad = (counter*6.28)/800.0; //dividing by 64 counts per revolution
+            newRad = (counter*6.28)/800.0; //dividing by 800 counts per revolution and multiplying by 2*PI to get radians
             Serial.println(newRad);
           }
           else {
             counter -= 1.0;
-            if(counter < 0.0){
+            if(counter < 0.0){                //resets counter if the number of counts exceeds 1 revolution in the opposite direction
               counter = 800.0;
             }
             newRad = (counter*6.28)/800.0;
             Serial.println(newRad);
           }
         
-          oldRad = newRad;    
+          oldRad = newRad;                  
         }
       prevStateCLK = currentStateCLK;  
     }
 //}
 
-void ISR_reset(){
+void ISR_reset(){                            //when push-button is HIGH, resets the desired angle to the starting position
   desiredAngle = 0.0;  
   Serial.println(desiredAngle);
   
 }
 
-void receiveQuad(int byteCount){
+void receiveQuad(int byteCount){            //function receives byte over I2C and performs a calculation to output the desired angle associated with the respective quadrant
   byte currentQuad;
   while(Wire.available()){
     currentQuad = Wire.read();
@@ -136,35 +135,4 @@ void receiveQuad(int byteCount){
   Serial.println(" radians");
 }
 
-/*
-void PIcontroller() {
-  // Put this code in a loop within the main code, probably only active when the wheel is not independently moving
-  // previous to the loop below, you should set desPos to the position that you wish to maintain
-  // Make sure that AngularPosition is updated before this point
-  newRad = -1*desiredAngle;//whatever you used for the angular position
-  //currPos=AngularPosition1;
-  
-  error=setPosition-;
-  
-  
-  I=I+(Ts/1000)*error;
-  
-  PWMOutput = error*(Kp+Ki*I);
-  
 
-  if(abs(PWMOutput)>255){
-    PWMOutput=constrain(PWMOutput,-1,1)*255;
-    error = constrain(error,-1,1)*min(255/Kp,abs(error));
-  }
- 
- 
-  digitalWrite (VS1, HIGH);
-  PWMOutput = abs(PWMOutput);
-
-  analogWrite(outPin, PWMOutput);
-  Ts=micros()-Tc;
-  Tc=micros();
-
-  
-}
-*/
