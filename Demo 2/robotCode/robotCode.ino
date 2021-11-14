1205f12af4dc2135c49d2cdce942c64a50dce5e2
bntriton08 <91344902+bntriton08@users.noreply.github.com>
2021-11-14 10:48:17 -0700

    Update robotCode.ino

--- Demo 2/robotCode/robotCode.ino
+++ Demo 2/robotCode/robotCode.ino
@@ -1,3 +1,7 @@
+#include<Wire.h>
+
+#define SLAVE_ADDR 0x04 // Address that the arduino is listening at
+
 #define M1_DIR 7
 #define M1_PWM 9
 #define M2_DIR 8
@@ -43,6 +47,9 @@ float rotationalVelocity;
 float rotationalVelocitySet;
 float rotationalPosition;
 
+//int state;
+float stateData[3];
+
 
 void setup(){
     Serial.begin(2000000);
@@ -64,6 +71,8 @@ void setup(){
     attachInterrupt(digitalPinToInterrupt(ENC_RA), encoderISR_R, CHANGE);
     attachInterrupt(digitalPinToInterrupt(ENC_LA), encoderISR_L, CHANGE);
 
+    Wire.begin(SLAVE_ADDR); // Start listening for I2C
+    Wire.onReceive(receiveState); // Interrupt handler for I2C input
     // TODO: initialize I2C stuff
 }
 
@@ -79,7 +88,7 @@ void loop(){
     if(voltageR >= 0) digitalWrite(M1_DIR, HIGH);
     else digitalWrite(M1_DIR, LOW);
     if(voltageL >= 0) digitalWrite(M2_DIR, HIGH);
-    else digitalWrite(M2_DIR, HIGH);
+    else digitalWrite(M2_DIR, LOW);
     analogWrite(M1_PWM, constrain(abs(voltageR), 0, 255));
     analogWrite(M2_PWM, constrain(abs(voltageL), 0, 255));
     while(micros() < startTime + period); // Delay for the rest of the period
@@ -104,7 +113,7 @@ void encoderISR_R(){
     if(digitalRead(ENC_RA) == digitalRead(ENC_RB))encoderCountR += 2;
     else encoderCountR -= 2;
 
-    angVelR = (float(encoderCountR - prevEncoderCountR)*((2.0*PI)/3200.0)*1000000)/(micros() - isrTimeR);
+    angVelR = (float(encoderCountR - prevEncoderCountR)*((2.0*PI)/3200.0)*1000000.0)/(micros() - isrTimeR);
     isrTimeR = micros();
     prevEncoderCountR = encoderCountR;
 }
@@ -113,7 +122,57 @@ void encoderISR_L(){
     if(digitalRead(ENC_LA) == digitalRead(ENC_LB))encoderCountL -= 2;
     else encoderCountL += 2;
     
-    angVelL = (float(encoderCountL - prevEncoderCountL)*((2.0*PI)/3200.0)*1000000)/(micros() - isrTimeL);
+    angVelL = (float(encoderCountL - prevEncoderCountL)*((2.0*PI)/3200.0)*1000000.0)/(micros() - isrTimeL);
     isrTimeL = micros();
     prevEncoderCountL = encoderCountL;
 }
+
+
+void receiveState(int byteCount){ 
+  for(int i=0; i<3; i++){
+    stateData[i] = Wire.read();
+    Serial.println(stateData[i]);  
+  }
+}
+/*
+
+void receiveState(int byteCount){
+  int i; // Initalize variable to hold new set position from I2C
+  while(Wire.available()){ // While the arduino is still getting data over I2C
+    i = Wire.read(); // Set the current quad byte to the byte that was sent over I2C
+  }
+  state = i;
+} */
+
+
+/* Switch statement for line detection
+switch(stateData[0]){
+      case 0:
+        if(stateData[1] == 0){
+          //seach for tape
+        }
+        else rotationalVelocitySet = -0.1; //turn car to the right, rotate CW?
+        break;
+      case 1:
+        if(stateData[1] == 0) rotationalVelocitySet = 0.1; //turn car to the left, rotate CCW
+        else rotationalVelocitySet = 0.0; //stop adjusting when car is centered on line
+        break;
+    }
+*/
+
+/*
+switch(state){
+      case 1: //robot doesn't see tape, rotate to search
+        rotationalVelocitySet = 7.5;
+        forwardVelocitySet = 0.0;
+      break;
+      case 2: //robot sees tape, move forward
+        rotationalVelocitySet = 0.0;
+        forwardVelocitySet = 0.5;
+      break;
+      case 3: //robot at tape, stop moving
+        rotationalVelocitySet = 0.0;
+        forwardVelocitySet = 0.0;
+      break;
+    }
+*/
