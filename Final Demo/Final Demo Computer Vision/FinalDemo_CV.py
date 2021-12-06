ece6aea57aabbd79ece59557544e09831e07902d
Taran Soden <91348650+taransoden@users.noreply.github.com>
2021-12-06 14:32:38 -0700

    Add comments to CV code

--- Final Demo/Final Demo Computer Vision/FinalDemo_CV.py
+++ Final Demo/Final Demo Computer Vision/FinalDemo_CV.py
@@ -18,21 +18,16 @@ import time
 import smbus2
 import board
 import busio
-#import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 
 # -- Global Variables --
-# CV
 recentPhi = 0.0
 recentDir = 0b01
 imwidth = 0
 imheight = 0
 
 ARDUINO_ADDRESS = 0x04
-#lcd_i2c = busio.I2C(board.SCL, board.SDA)
-#lcd = character_lcd.Character_LCD_RGB_I2C(lcd_i2c, 16, 2)
 bus = smbus2.SMBus(1)
-#lcd.color = [0, 100, 0]
 
 # --- Function definitions ---
 '''
@@ -50,18 +45,11 @@ def sendSecondary(direction, inFrame, angle):
     print("Angle: %d" % round(angle))
     print("Direction: %d" % direction)
     
-    array = [direction, sign, round(absPhi)]
-    #print("Direction: %d   Angle: %d   RecentDir: %d" % (array[0], (-1 * array[1])*array[2], recentDir))
+    array = [direction, sign, round(absPhi)
     try:
         bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, array)
     except:
         print("I2C connection failed, please check connection")
-        
-    #lcd.clear()
-    #if inFrame:
-    #    lcd.message = "Angle:\n" + str(angle)
-    #else:
-    #    lcd.message = "Searching..."
 
 
 '''
@@ -90,7 +78,7 @@ Output:    Color masked image
 '''
 def filter_color(fullImage):
     imgHSV = cv.cvtColor(fullImage, cv.COLOR_BGR2HSV)                            # Reformat image to HSV
-    mask = cv.inRange(imgHSV, np.array([95,100,5]), np.array([125,255,255]))    # Filter blue
+    mask = cv.inRange(imgHSV, np.array([95,100,5]), np.array([125,255,255]))     # Filter blue
     maskedHSV = cv.bitwise_and(imgHSV, imgHSV, mask = mask)                      # Apply mask
     maskedImage = cv.cvtColor(maskedHSV, cv.COLOR_HSV2BGR)                       # Convert back to BGR
     return maskedImage
@@ -105,7 +93,7 @@ Output:    Original frame with/without contours
 def detect_shape(maskedImage, originalImage):
     grayMasked = cv.cvtColor(maskedImage, cv.COLOR_BGR2GRAY)     # Convert to gray
     blurred = cv.GaussianBlur(grayMasked, (5,5), 0)              # Blur
-    _, thresh = cv.threshold(blurred, 5, 255, cv.THRESH_BINARY) # Make threshold    
+    _, thresh = cv.threshold(blurred, 5, 255, cv.THRESH_BINARY)  # Make threshold    
     (_, contours, _) = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # Find any shape contours
     angle = 0
     follow = True
@@ -119,26 +107,19 @@ def detect_shape(maskedImage, originalImage):
             perimeter = cv.arcLength(c, True) # approximate the shape
             approx = cv.approxPolyDP(c, 0.01*perimeter, True)
             
-            # determine if the tape is present
+            # If a 12 sided shape is detected (a cross) then send the command to full stop the robot
             if len(approx) == 12:
                 sendSecondary(4, True, angle)
-            
+             
+            # Find tape points
             bottom = tuple(c[c[:,:,1].argmax()][0])
             top = tuple(c[c[:, :, 1].argmin()][0])
-            #left = tuple(c[c[:, :, 0].argmin()][0])
-            #right = tuple(c[c[:, :, 0].argmax()][0])
-            #cx = int(M['m10']/M['m00']) # Find position of contour
-            #cy = int(M['m01']/M['m00'])
-            
             cx = int(top[0])
             cy = int(top[1])
             
-            cv.drawContours(originalImage, [c], 0, (0,0,255), 5)          # Draw contour
-#            cv.circle(image, left, 8, (0, 50, 255), -1)
-#            cv.circle(image, right, 8, (0, 255, 255), -1)
-#            cv.circle(image, top, 8, (255, 50, 0), -1)
-#            cv.circle(image, bottom, 8, (255, 255, 0), -1)
-#            
+            cv.drawContours(originalImage, [c], 0, (0,0,255), 5) # Draw contour
+             
+            # Determine the angle of the tape to send
             lastPhi = recentPhi
             angle = det_angle(cx, imwidth)
             
@@ -146,17 +127,18 @@ def detect_shape(maskedImage, originalImage):
             if angle != lastPhi:
                 # Determine direction to travel to follow the line
                 if cx >= (3/5)*imwidth:
-                    # | | | |x|x|
+                    # | | | |x|x|   Turn right
                     recentDir = 0b11
                     sendSecondary(0b11, True, angle)
                 elif (cx <= (3/5)*imwidth) and (cx > (2/5)*imwidth):
-                    # | | |x| | |
+                    # | | |x| | |   Go straight
                     recentDir = 0b11
                     sendSecondary(0b11, True, angle)
                 elif cx < (2/5)*imwidth:
-                    # |x|x| | | |
+                    # |x|x| | | |   Turn left
                     recentDir = 0b11
                     sendSecondary(0b11, True, angle)
+             
         elif recentDir != 0b00:
             recentDir = 0b00
             sendSecondary(0b00, False, angle)
